758d4d8157a33f9e3f1090b1bee755965a2f8236
Елена Рыбкина <rybkina01@list.ru>
2024-11-01 15:02:25 +0300

    add greeting whit name

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,1 +1,5 @@
+import { userName } from '/home/rybkina01/qa-auto-engineer-javascript-project-44/src/cli.js';
+
 console.log('Welcome to the Brain Games!');
+
+console.log(`Hello, ${userName}!`);
---
+++ src/cli.js
@@ -0,0 +1,3 @@
+import readlineSync from 'readline-sync';
+
+export let userName = readlineSync.question('May I have your name? ');
