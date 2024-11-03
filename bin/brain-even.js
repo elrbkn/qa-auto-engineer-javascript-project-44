8a3d2cb5cea575063f170c0a45ccbc242495cf9a
Елена Рыбкина <rybkina01@list.ru>
2024-11-03 17:39:26 +0300

    eslint fix brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,46 +1,44 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
-//приветствие
+// приветствие
 console.log('Welcome to the Brain Games!');
 const userName = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${userName}!`);
 
-//поясняем смысл
+// поясняем смысл
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-//цикл подсчета
+// цикл подсчета
 let correctAnswersCount = 0;
 
 while (correctAnswersCount < 3) {
-    //получаем число от 1 до 100
-    let getRandomNumber = Math.floor(Math.random() * 100) + 1;
-    //выводим число
-    console.log('Question: ' + getRandomNumber);
-    //получаем ответ пользователя
-    let getAnswer = readlineSync.question('Your answer: ');
+  // получаем число от 1 до 100
+  const getRandomNumber = Math.floor(Math.random() * 100) + 1;
+  // выводим число
+  console.log(`Question: ${getRandomNumber}`);
+  // получаем ответ пользователя
+  const getAnswer = readlineSync.question('Your answer: ');
 
-    //проверяем четность
-    const checkEvenness = (number) => {
-        return number % 2 === 0 ? 'yes' : 'no'; // Возвращаем 'yes', если четное, иначе 'no'
-    };
+  // проверяем четность
+  const checkEvenness = (number) => (number % 2 === 0 ? 'yes' : 'no') // Возвращаем 'yes', если четное, иначе 'no'
+    ;
 
-    // Получаем правильный ответ
-    const correctAnswer = checkEvenness(getRandomNumber); 
-    //сравниваем ответы
-    if (getAnswer === correctAnswer) {
-        console.log('Correct!');
-        correctAnswersCount++;
-            // Проверяем, достигли ли мы 3 правильных ответа
-            if (correctAnswersCount === 3) {
-                console.log(`Congratulations, ${userName}!`);
-                //закрываем цикл
-                break;
-            }
-        } else {
-        console.log(`Answer "${getAnswer}" if the number is even, otherwise answer "${correctAnswer}".`);
-        //закрываем цикл
-        break;
-        }
-};
-
+  // Получаем правильный ответ
+  const correctAnswer = checkEvenness(getRandomNumber);
+  // сравниваем ответы
+  if (getAnswer === correctAnswer) {
+    console.log('Correct!');
+    correctAnswersCount++;
+    // Проверяем, достигли ли мы 3 правильных ответа
+    if (correctAnswersCount === 3) {
+      console.log(`Congratulations, ${userName}!`);
+      // закрываем цикл
+      break;
+    }
+  } else {
+    console.log(`Answer "${getAnswer}" if the number is even, otherwise answer "${correctAnswer}".`);
+    // закрываем цикл
+    break;
+  }
+}
