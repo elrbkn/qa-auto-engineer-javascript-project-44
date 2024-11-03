d36fa6a2bc6ee512929fa1b3f8164b32031885a2
Елена Рыбкина <rybkina01@list.ru>
2024-11-03 19:39:54 +0300

    mini update

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,10 +1,9 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
+import { greeting } from '../index.js';
 
 // приветствие
-console.log('Welcome to the Brain Games!');
-const userName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${userName}!`);
+greeting ();
 
 // поясняем смысл
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
