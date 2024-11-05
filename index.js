04cdb4511e2426fbd3c30fd2997659737e828b05
Елена Рыбкина <rybkina01@list.ru>
2024-11-05 20:31:06 +0300

    update index.js games/brain-progression.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ export const greeting = () => {
 export const getRandomNumber = () => Math.floor(Math.random() * 100) + 1;
 
 // Функция для проверки четности, 'yes' если четное, иначе 'no'
-export const checkEvenness = (number) => number % 2 === 0 ? 'yes' : 'no';
+export const checkEvenness = (number) => (number % 2 === 0 ? 'yes' : 'no');
 
 // Функция подсчета ответов
 export const handleAnswer = (userAnswer, correctAnswer, userName, correctAnswersCount, incorrectMessage) => {
@@ -21,14 +21,14 @@ export const handleAnswer = (userAnswer, correctAnswer, userName, correctAnswers
     console.log('Correct!');
     correctAnswersCount += 1;
     if (correctAnswersCount === 3) {
-      // Завершение игры при достижении 3      
+      // Завершение игры при достижении 3
       console.log(`Congratulations, ${userName}!`);
       return { correctAnswersCount, finished: true };
     }
   } else {
-      // Завершение игры при неправильном ответе
-      console.log(incorrectMessage);
-      return { correctAnswersCount, finished: true };
+    // Завершение игры при неправильном ответе
+    console.log(incorrectMessage);
+    return { correctAnswersCount, finished: true };
   }
   // Игра продолжается
   return { correctAnswersCount, finished: false };
@@ -42,9 +42,9 @@ export const getRandomOperator = () => {
   return randomOperator;
 };
 
-// Функция вычисления результата математической операции 
+// Функция вычисления результата математической операции
 export const getResult = (num1, operator, num2) => {
-  //Определяем значение оператора
+  // Определяем значение оператора
   switch (operator) {
     case '+':
       return num1 + num2;
@@ -57,11 +57,11 @@ export const getResult = (num1, operator, num2) => {
 
 // Функция вычисления НОД
 export const gcd = (num1, num2) => {
-  //Алгоритм Евклида
+  // Алгоритм Евклида
   while (num2 !== 0) {
-      let temp = num2;
-      num2 = num1 % num2;
-      num1 = temp;
+    const temp = num2;
+    num2 = num1 % num2;
+    num1 = temp;
   }
   return num1;
 };
@@ -74,13 +74,11 @@ export const generateRandomArray = () => {
   // Создаем массив
   const array = [];
   // Заполняем массив 10 числами
-  for (let i = 0; i < 10; i++) {
-      array.push(start + (i * step));
+  for (let i = 0; i < 10; i += 1) {
+    array.push(start + (i * step));
   }
   return array;
 };
 
 // Функция для изменнения значения в массиве
-export const replaceValueInArray = (array, oldValue, newValue) => {
-  return array.map(item => item === oldValue ? newValue : item)
-};
+export const replaceValueInArray = (array, oldValue, newValue) => array.map((item) => (item === oldValue ? newValue : item));
