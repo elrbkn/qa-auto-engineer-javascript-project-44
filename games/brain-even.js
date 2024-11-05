ee93dfc4df1a04f27094c78a0563aa268d0f331d
Елена Рыбкина <rybkina01@list.ru>
2024-11-05 19:53:02 +0300

    fix eslint problems

--- games/brain-even.js
+++ games/brain-even.js
@@ -1,28 +1,30 @@
 import readlineSync from 'readline-sync';
-import { greeting, getRandomNumber, checkEvenness, handleAnswer } from '../index.js';
+import {
+  greeting, getRandomNumber, checkEvenness, handleAnswer,
+} from '../index.js';
 // brain-even
 export const playGameEven = () => {
-    //Приветствуем
-    const userName = greeting();
-    // Поясняем смысл
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
-    // Цикл игры
-    let correctAnswersCount = 0;
-    while (correctAnswersCount < 3) {
-      // Получаем число
-      const number = getRandomNumber();
-      console.log(`Question: ${number}`);
-      // Получаем ответ пользователя
-      const userAnswer = readlineSync.question('Your answer: ');
-      // Проверяем четность
-      const correctAnswer = String(checkEvenness(number));
-      // Сверяем результаты
-      const incorrectMessage = `Answer "${userAnswer}" if the number is even, otherwise answer "${correctAnswer}".\nLet's try again, ${userName}!`;
-      const result = handleAnswer(userAnswer, correctAnswer, userName, correctAnswersCount, incorrectMessage);
-      correctAnswersCount = result.correctAnswersCount;
-      // Закрываем цикл
-      if (result.finished) {
-        break;
-      }
+  // Приветствуем
+  const userName = greeting();
+  // Поясняем смысл
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  // Цикл игры
+  let correctAnswersCount = 0;
+  while (correctAnswersCount < 3) {
+    // Получаем число
+    const number = getRandomNumber();
+    console.log(`Question: ${number}`);
+    // Получаем ответ пользователя
+    const userAnswer = readlineSync.question('Your answer: ');
+    // Проверяем четность
+    const correctAnswer = String(checkEvenness(number));
+    // Сверяем результаты
+    const incorrectMessage = `Answer "${userAnswer}" if the number is even, otherwise answer "${correctAnswer}".\nLet's try again, ${userName}!`;
+    const result = handleAnswer(userAnswer, correctAnswer, userName, correctAnswersCount, incorrectMessage);
+    correctAnswersCount = result.correctAnswersCount;
+    // Закрываем цикл
+    if (result.finished) {
+      break;
     }
-  };
+  }
+};
