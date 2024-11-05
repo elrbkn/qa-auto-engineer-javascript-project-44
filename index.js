bb4d37a3a732310fe2e9e501bf52af8b32c970b9
Елена Рыбкина <rybkina01@list.ru>
2024-11-05 23:26:24 +0300

    eslint --fix

--- index.js
+++ index.js
@@ -67,7 +67,9 @@ export const getResult = (num1, operator, num2) => {
 };
 
 // Функция вычисления НОД
-export const gcd = (num1, num2) => {
+export const gcd = (a, b) => {
+  let num1 = a;
+  let num2 = b;
   // Алгоритм Евклида
   while (num2 !== 0) {
     const temp = num2;
